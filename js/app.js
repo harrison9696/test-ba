b04526a1e637e7c46facd746ce6be45a4177a43e
Harrison Quadir <harrison@harrisons-mbp.home>
2021-01-05 21:59:04 +0000

    reuploaded project to github

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('.navbar__hamburger');
+const closeBtn = document.querySelector('.nav-open__close-btn');
+const navOpen = document.querySelector('.nav-open');
+
+hamburger.addEventListener('click', () => {
+  navOpen.classList.add('nav-active');
+});
+
+closeBtn.addEventListener('click', () => {
+  navOpen.classList.remove('nav-active');
+});
